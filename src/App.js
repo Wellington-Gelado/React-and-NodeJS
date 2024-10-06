8c7888698858409d40255c31b9dd771129475ac0
Wellington <wellingtonsantos1996@usp.br>
2024-10-06 11:46:46 -0300

    Logo e nome do site, sem estilização.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './imagens/logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="logo">
+            <img src={logo} alt="logo"></img>
+            <p>Alura Books</p>
+        </div>
         
       </header>
     </div>
